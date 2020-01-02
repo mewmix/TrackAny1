7a5ca764a836245e0deb54b7916036a947ed6b62
Mark Faulkner <markfaulk350@gmail.com>
2020-01-01 21:29:58 -0800

    finalizing oauth and worked on different schema design

--- schema.sql
+++ schema.sql
@@ -16,7 +16,70 @@ CREATE TABLE users(
     created TIMESTAMP DEFAULT NOW()
 );
 
-INSERT INTO `auth_test_db`.`users` (`fName`, `lName`, `email`, `password`) VALUES ('Mark', 'Faulkner', 'markfaulk350@gmail.com', 'password');
+CREATE TABLE trackers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    trkType VARCHAR (255) NOT NULL,
+    trkLink VARCHAR (255) NOT NULL UNIQUE,
+    lastLocationPing INT UNSIGNED DEFAULT 0,
+    lastApiCall INT UNSIGNED DEFAULT 0,
+    owner_id INT NOT NULL, 
+    created TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE pings(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    unixTime INT UNSIGNED NOT NULL,
+    lat DECIMAL(11, 8) NOT NULL,
+    lng DECIMAL(12, 8) NOT NULL,
+    alt INT,
+    agl VARCHAR(255) DEFAULT 'n/a',
+    velocity VARCHAR(255) DEFAULT 'n/a',
+    heading VARCHAR(255) DEFAULT 'n/a',
+    txtMsg VARCHAR(255),
+    isEmergency VARCHAR(255),
+    tracker_id INT,
+    UNIQUE (unixTime, tracker_id),
+    FOREIGN KEY (tracker_id) REFERENCES trackers(id) ON DELETE CASCADE
+);
+
+CREATE TABLE flying_groups(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    creatorId VARCHAR (255) NOT NULL,
+    groupName VARCHAR (255) NOT NULL UNIQUE,
+    region VARCHAR (255) DEFAULT '',
+    info VARCHAR (255) DEFAULT '',
+    radioFrq VARCHAR (255) DEFAULT '',
+    created TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE groups_have_members(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    created TIMESTAMP DEFAULT NOW(),
+    group_id INT NOT NULL,
+    member_id INT NOT NULL,
+    UNIQUE (group_id, member_id),
+    FOREIGN KEY (group_id) REFERENCES flying_groups(id) ON DELETE CASCADE,
+    FOREIGN KEY (member_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
