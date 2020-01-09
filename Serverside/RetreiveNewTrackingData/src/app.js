13521768b6a534a724ebbb2446deaecfc4d2e459
Mark Faulkner <markfaulk350@gmail.com>
2020-01-09 14:34:21 -0800

    added mock users and trackers to test

--- Serverside/RetreiveNewTrackingData/src/app.js
+++ Serverside/RetreiveNewTrackingData/src/app.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+
 const TrackersServices = require('./services/trackers_services');
 
-exports.handler = async (event, context) => {
-    try {
-        const allTrackers = await TrackersServices.getAllTrackers();
-        console.log(allTrackers);
-    } catch (err) {
-        console.log(err);
-    }
-}
+// exports.handler = async (event, context) => {
+//     try {
+//         const allTrackers = await TrackersServices.getAllTrackers();
+//         console.log(allTrackers);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// TrackersServices.getAllTrackers().then((res) => {
+//     console.log(res);
+// }).catch((err) => {
+//     console.log(err);
+// });
+
+// I need to pu in some mock trackers
