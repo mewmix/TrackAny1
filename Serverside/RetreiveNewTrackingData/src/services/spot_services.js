3b49c0cd8a74da855eefe7401d5017f8e91e1dfa
Mark Faulkner <markfaulk350@gmail.com>
2020-01-09 21:26:46 -0800

    roughly finished spot service function

--- Serverside/RetreiveNewTrackingData/src/services/spot_services.js
+++ Serverside/RetreiveNewTrackingData/src/services/spot_services.js
@@ -1,9 +1,26 @@
 const axios = require('axios');
+const dateFormat = require('dateformat');
 
-// This needs to return a string containing sql insert statements.
+
 async function getTrackingData(deviceID, trkLink) {
-    const data = await axios.get(trkLink);
-    console.log(data);
+    const weekAgo = new Date(Date.now() - 604800000);
+    const spotFormatedDate = dateFormat(weekAgo, 'isoDateTime');
+
+    const finalURL = `${trkLink}/message.json?startDate=${spotFormatedDate}`;
+
+    const res = await axios.get(finalURL);
+
+    const dataPoints = res.data.response.feedMessageResponse.messages.message;
+
+    let insertStatement = '';
+
+    for (let p of dataPoints) {
+        let message = '';
+        if (p.messageContent !== undefined) { message = p.messageContent }
+        insertStatement = insertStatement.concat(`(${p.unixTime}, ${p.latitude}, ${p.longitude}, ${p.altitude}, "n/a", "n/a", "${message}", "n/a", ${deviceID}),`);
+    }
+
+    return insertStatement;
 }
 
 module.exports.getTrackingData = getTrackingData;
--- Serverside/RetreiveNewTrackingData/src/testSpot.js
+++ Serverside/RetreiveNewTrackingData/src/testSpot.js
@@ -1,5 +1,26 @@
+const axios = require('axios');
+const dateFormat = require('dateformat');
+
 async function testSpot(deviceID, trkLink) {
 
+    const weekAgo = new Date(Date.now() - 604800000);
+    const spotFormatedDate = dateFormat(weekAgo, 'isoDateTime');
+
+    const finalURL = `${trkLink}/message.json?startDate=${spotFormatedDate}`;
+
+    const res = await axios.get(finalURL);
+
+    const dataPoints = res.data.response.feedMessageResponse.messages.message;
+
+    let insertStatement = '';
+
+    for (let p of dataPoints) {
+        let message = '';
+        if (p.messageContent !== undefined) { message = p.messageContent }
+        insertStatement = insertStatement.concat(`(${p.unixTime}, ${p.latitude}, ${p.longitude}, ${p.altitude}, "n/a", "n/a", "${message}", "n/a", ${deviceID}),`);
+    }
+
+    return insertStatement;
 }
 
-testSpot('', '');
+testSpot(4, 'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/0lOSqK4ZMcY7h6ulQ936SqUeQqSTlNHDa');
