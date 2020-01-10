08d13b0266e5a14ed5b4ec411da37887a7865905
Mark Faulkner <markfaulk350@gmail.com>
2020-01-09 22:45:22 -0800

    trying out different things with garmin service

--- Serverside/RetreiveNewTrackingData/src/services/garmin_services.js
+++ Serverside/RetreiveNewTrackingData/src/services/garmin_services.js
@@ -1,9 +1,14 @@
 const axios = require('axios');
+const dateFormat = require('dateformat');
 
-// This needs to return a string containing sql insert statements.
 async function getTrackingData(deviceID, trkLink) {
-    const data = await axios.get(trkLink);
-    console.log(data);
+    const weekAgo = new Date(Date.now() - 604800000);
+    dateFormat.masks.customgarmin = 'HH:MM! "Can\'t touch this!"';
+    const garminFormatedDate = dateFormat(weekAgo, 'customgarmin');
+
+    const finalURL = `${trkLink}?d1=${garminFormatedDate}`;
+
+    const res = await axios.get(finalURL);
 }
 
 module.exports.getTrackingData = getTrackingData;
--- Serverside/RetreiveNewTrackingData/src/testGarmin.js
+++ Serverside/RetreiveNewTrackingData/src/testGarmin.js
@@ -1,5 +1,24 @@
-async function testGarmin(deviceID, trkLink) {
+const axios = require('axios');
+const dateFormat = require('dateformat');
+const xml2js = require('xml2js');
+const util = require('util');
 
+async function testGarmin(deviceID, trkLink) {
+    const weekAgo = new Date(Date.now() - (604800000 * 2));
+    dateFormat.masks.garmin = 'yyyy-mm-dd"T"HH:MM"z"';
+    const garminFormatedDate = dateFormat(weekAgo, 'garmin');
+
+    const finalURL = `${trkLink}?d1=${garminFormatedDate}`;
+    console.log(finalURL);
+
+    const res = await axios.get(finalURL);
+
+    const parser = new xml2js.Parser();
+
+    const rawData = await parser.parseStringPromise(res.data);
+    console.log(util.inspect(rawData.kml.Document[0].Folder[0].Placemark[0], false, null));
+
+    //console.log(res);
 }
 
-testGarmiin('', '');
+testGarmin(2, 'https://us0.inreach.garmin.com/Feed/Share/bradstevenson');
