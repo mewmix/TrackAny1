7a3693d7382b2e75240bd3254e995173989d2202
Mark Faulkner <markfaulk350@gmail.com>
2020-01-12 00:17:54 -0800

    was able to get all of a groups tracking data but the code is terrible and i will fix it tommorow

--- Serverside/API/src/controllers/groups_controller.js
+++ Serverside/API/src/controllers/groups_controller.js
@@ -4,8 +4,53 @@ module.exports = {
     async getGroupTrackingData(req, res) {
         try {
             const { id, timespan } = req.params;
-            const groupTrackingData = await GroupsServices.getGroupTrackingData(id, timespan);
-            return res.status(200).json({ groupTrackingData });
+            const currentUnix = Math.floor(Date.now() / 1000);
+            let queryTill;
+
+            switch (timespan) {
+                case 'mostrecent':
+                    console.log("Need to get most recent")
+                    break;
+                case '1hr':
+                    queryTill = currentUnix - 3600;
+                    break;
+                case '2hr':
+                    queryTill = currentUnix - 7200;
+                    break;
+                case '6hr':
+                    queryTill = currentUnix - 21600;
+                    break;
+                case '12hr':
+                    queryTill = currentUnix - 43200;
+                    break;
+                case '24hr':
+                    queryTill = currentUnix - 86400;
+                    break;
+                case '48hr':
+                    queryTill = currentUnix - 172800;
+                    break;
+                case '1week':
+                    queryTill = currentUnix - 604800;
+                    break;
+                case '2weeks':
+                    queryTill = currentUnix - 1209600;
+                    break;
+                case 'all':
+                    queryTill = 1;
+                    break;
+                default:
+                    return res.status(400).json({ error: e, message: 'Failed to get group tracking data bad request' });
+            }
+
+            const groupMembers = await GroupsServices.getGroupRoster(id);
+
+            let allData = [];
+            for (let member of groupMembers) {
+                const userTrackingData = await GroupsServices.getUserTrackingData(member.id, queryTill);
+                allData.push({member: member, userTrackingData});
+            }
+
+            return res.status(200).json({ allData });
         } catch (e) {
             return res.status(500).json({ error: e, message: 'Failed to get group tracking data' });
         }
--- Serverside/API/src/services/groups_services.js
+++ Serverside/API/src/services/groups_services.js
@@ -1,76 +1,26 @@
 const db = require('../db');
 
 module.exports = {
-    getGroupTrackingData(groupID, timeSpan) {
+    getUserTrackingData(userID, queryTill) {
         return new Promise((resolve, reject) => {
-
-            const currentUnix = Math.floor(Date.now() / 1000);
-
-            let queryTill;
-
-            switch (timeSpan) {
-                case 'mostrecent':
-                    console.log("Need to get most recent")
-                    break;
-                case '1hr':
-                    queryTill = currentUnix - 3600;
-                    break;
-                case '2hr':
-                    queryTill = currentUnix - 7200;
-                    break;
-                case '6hr':
-                    queryTill = currentUnix - 21600;
-                    break;
-                case '12hr':
-                    queryTill = currentUnix - 43200;
-                    break;
-                case '24hr':
-                    queryTill = currentUnix - 86400;
-                    break;
-                case '48hr':
-                    queryTill = currentUnix - 172800;
-                    break;
-                case '1week':
-                    queryTill = currentUnix - 604800;
-                    break;
-                case '2weeks':
-                    queryTill = currentUnix - 1209600;
-                    break;
-                case 'all':
-                    queryTill = 1;
-                    break;
-                default:
-                    reject(new Error('did not specify time span for group tracking data'));
-            }
-
-            let sqlStatement = 'SELECT users.id, fName, lName, picture FROM groups_have_members INNER JOIN users ON groups_have_members.member_id = users.id WHERE groups_have_members.group_id=?;';
-
-            db.query(sqlStatement, groupID, (err, groupMembers) => {
+            let sqlStatement = 'SELECT id, unixTime, lat, lng, alt, agl, velocity, heading, txtMsg, isEmergency FROM pings WHERE user_id=? AND unixTime >=? ORDER BY unixTime DESC;';
+            db.query(sqlStatement, [userID, queryTill], (err, rows) => {
                 if (err) reject(err);
                 else {
-                    let groupTrackingData = []
-                    for(let member of groupMembers){
-                        let sqlStatement = 'SELECT id, unixTime, lat, lng, alt, agl, velocity, heading, txtMsg, isEmergency FROM pings WHERE user_id=? AND unixTime >=? ORDER BY unixTime DESC;';
-                        db.query(sqlStatement, [member.id, queryTill], (err, rows) => {
-                            if (err) reject(err);
-                            else {
-                            }
-                        });
-                    }
-                    
+                    resolve(rows);
                 }
             });
         });
     },
-    // getGroupRoster(groupID) {
-    //     return Promise((resolve, reject) => {
-    //         let sqlStatement = 'SELECT users.id, fName, lName, picture FROM groups_have_members INNER JOIN users ON groups_have_members.member_id = users.id WHERE groups_have_members.group_id=?;';
-    //         db.query(sqlStatement, groupID, (err, rows) => {
-    //             if (err) reject(err);
-    //             else {
-    //                 resolve(rows);
-    //             }
-    //         });
-    //     });
-    // }
+    getGroupRoster(groupID) {
+        return new Promise((resolve, reject) => {
+            let sqlStatement = 'SELECT users.id, fName, lName, picture FROM groups_have_members INNER JOIN users ON groups_have_members.member_id = users.id WHERE groups_have_members.group_id=?;';
+            db.query(sqlStatement, groupID, (err, rows) => {
+                if (err) reject(err);
+                else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
 }
