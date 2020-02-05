7e57efb63293834f7fc517fc1e743a451f85ee16
Mark Faulkner <markfaulk350@gmail.com>
2020-02-05 01:32:36 -0800

    added google oauth redirect to client side app

--- Serverside/API/src/routes/authRoutes.js
+++ Serverside/API/src/routes/authRoutes.js
@@ -6,7 +6,8 @@ module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
     app.get('/auth/google/callback', passport.authenticate('google', { session: false }), (req, res) => {   // We can also add a successRedirect:'/something' and a failureRedirect:'/something'
-        res.status(200).json({ status: "Nominal!", description: "Google Oauth was a Success", userData: req.user });
+        // res.status(200).json({ status: "Nominal!", description: "Google Oauth was a Success", userData: req.user });
+        res.redirect(`https://trackany1.com/oauth2/callback#access_token=${req.user}`);
     });
 
     // Facebook
