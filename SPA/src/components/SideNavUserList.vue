874c12587536c067feb89f97aa1ce54153af71b6
Mark Faulkner <markfaulk350@gmail.com>
2020-04-13 06:55:52 -0700

    added custom css animated infinite flip glider for an error page

--- SPA/src/components/SideNavUserList.vue
+++ SPA/src/components/SideNavUserList.vue
@@ -1,10 +1,15 @@
 <template>
-  <v-list class="pt-0" v-if="trackingData.length > 0">
-    <SideNavUserListItem v-for="user in trackingData" :key="user.id" :user="user"></SideNavUserListItem>
-  </v-list>
-  <v-list v-else class="pt-5">
-    <v-list-item>There is no tracking data for this given time period.</v-list-item>
-  </v-list>
+  <div>
+    <v-list class="pt-0" v-if="trackingData.length > 0">
+      <SideNavUserListItem v-for="user in trackingData" :key="user.id" :user="user"></SideNavUserListItem>
+    </v-list>
+    <div v-else>
+      <v-list class="pt-5 mb-10">
+        <v-list-item>There is no tracking data for this given time period.</v-list-item>
+      </v-list>
+      <v-img id="glider" :src="require('@/assets/glider.svg')"></v-img>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -20,3 +25,20 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+#glider {
+  margin: 0 auto;
+  width: 100px;
+  animation: rotation 1.8s infinite linear;
+}
+
+@keyframes rotation {
+    0% {
+      transform: rotate(359deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+}
+</style>
