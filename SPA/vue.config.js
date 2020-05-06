69910f6252f762be39c3939dcca398148902f251
Mark Faulkner <markfaulk350@gmail.com>
2020-05-06 13:09:52 -0700

    trying to change home screen icon

--- SPA/vue.config.js
+++ SPA/vue.config.js
@@ -5,7 +5,26 @@ module.exports = {
 
   pwa: {
     name: 'TrackAny1',
-    themeColor: '#BD10E0'
+    themeColor: '#BD10E0',
+    // icons: [
+    //   {
+    //     'src': './img/icons/android-chrome-192x192.png',
+    //     'sizes': '192x192',
+    //     'type': 'image/png',
+    //   },
+    //   {
+    //     'src': './img/icons/android-chrome-512x512.png',
+    //     'sizes': '512x512',
+    //     'type': 'image/png',
+    //   }
+    // ],
+    // iconPaths: {
+    //   favicon32: 'img/icons/favicon-32x32.png',
+    //   favicon16: 'img/icons/favicon-16x16.png',
+    //   appleTouchIcon: 'img/icons/apple-touch-icon-180x180.png',
+    //   maskIcon: 'img/icons/safari-pinned-tab.svg',
+    //   msTileImage: 'img/icons/mstile-144x144.png',
+    // }
   },
 
   lintOnSave: false
